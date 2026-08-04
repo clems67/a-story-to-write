aafeff01c12e2ed93a02184ad7260b35cda8d645
CCaravita <caravita.clement@gmail.com>
2026-08-04 14:33:52 +0200

    export code to StoryBlock

---
+++ src/lib/components/StoryBlock.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	
+	import SendHorizontal from '@lucide/svelte/icons/send-horizontal';
+	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
+
+    export let story
+    export let question
+
+	let loading = false;
+	function load() {
+		loading = true;
+	}
+</script>
+
+<p class="p-5">{story.text}</p>
+
+<div class="flex items-start justify-center">
+	<form class="pr-3">
+		<textarea class="textarea w-xl" rows="5" placeholder={question.before_input}></textarea>
+	</form>
+
+	{#if loading}
+		<LoadingSpinner />
+	{:else}
+		<button type="button" class="btn preset-filled" onclick={load}>
+			<SendHorizontal />
+		</button>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,12 @@
 <script lang="ts">
-	import story from '$lib/text/story.json';
 	import questions from '$lib/text/questions.json';
-	import SendHorizontal from '@lucide/svelte/icons/send-horizontal';
-	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
+	import story from '$lib/text/story.json';
+	import StoryBlock from '$lib/components/StoryBlock.svelte';
 
-	let loading = false;
-	function load() {
-		loading = true;
-	}
+	let theStory = story.find(x => x.id == 0)
+	let theQuestion = questions.find(x => x.id == theStory?.question_id)
+
+
 </script>
 
-<p class="p-5">{story[0].text}</p>
-
-<div class="flex items-start justify-center">
-	<form class="pr-3">
-		<textarea class="textarea w-xl" rows="5" placeholder={questions[0].before_input}></textarea>
-	</form>
-
-	{#if !loading}
-		<button type="button" class="btn preset-filled" onclick={load}>
-			<SendHorizontal />
-		</button>
-	{/if}
-
-	{#if loading}
-		<LoadingSpinner />
-	{/if}
-</div>
+<StoryBlock story={theStory} question={theQuestion} />
