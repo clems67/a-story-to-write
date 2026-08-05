63eccb9ab6c9101004b7146643152b714002ea7e
CCaravita <caravita.clement@gmail.com>
2026-08-05 12:43:09 +0200

    add hugging face inference for classification

--- src/lib/components/StoryBlock.svelte
+++ src/lib/components/StoryBlock.svelte
@@ -1,28 +1,42 @@
 <script lang="ts">
-	
 	import SendHorizontal from '@lucide/svelte/icons/send-horizontal';
 	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
 
-    export let story
-    export let question
+	async function llm_classifier(user_text: string, question_id: number) {
+		const response = await fetch('/api/classify', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ user_text, question_id })
+		});
 
-	let loading = false;
-	function load() {
+		if (!response.ok) throw new Error(await response.text());
+
+		return response.json();
+	}
+
+    let props = $props()
+	let input = $state('')
+	let loading = $state(false);
+
+	function resolve_input() {
 		loading = true;
+		llm_classifier(input, props.question.id)
+			.then((result) => console.log(result))
+			.finally(() => (loading = false));
 	}
 </script>
 
-<p class="p-5">{story.text}</p>
+<p class="p-5">{props.story.text}</p>
 
 <div class="flex items-start justify-center">
 	<form class="pr-3">
-		<textarea class="textarea w-xl" rows="5" placeholder={question.before_input}></textarea>
+		<textarea class="textarea w-xl" rows="5" placeholder={props.question.before_input} bind:value={input}></textarea>
 	</form>
 
 	{#if loading}
 		<LoadingSpinner />
 	{:else}
-		<button type="button" class="btn preset-filled" onclick={load}>
+		<button type="button" class="btn preset-filled" onclick={resolve_input}>
 			<SendHorizontal />
 		</button>
 	{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,8 @@
 	import story from '$lib/text/story.json';
 	import StoryBlock from '$lib/components/StoryBlock.svelte';
 
-	let theStory = story.find(x => x.id == 0)
-	let theQuestion = questions.find(x => x.id == theStory?.question_id)
-
-
+	let theStory = story.find((x) => x.id == 0);
+	let theQuestion = questions.find((x) => x.id == theStory?.question_id);
 </script>
 
 <StoryBlock story={theStory} question={theQuestion} />
