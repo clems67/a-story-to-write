822055a03b92ff4b8efa91ebd0f37b410eedd82b
CCaravita <caravita.clement@gmail.com>
2026-08-05 13:23:22 +0200

    export logic into StoryBlock

--- src/lib/components/StoryBlock.svelte
+++ src/lib/components/StoryBlock.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import SendHorizontal from '@lucide/svelte/icons/send-horizontal';
 	import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
+	import questions from '$lib/text/questions.json';
+	import stories from '$lib/text/story.json';
 
 	async function llm_classifier(user_text: string, question_id: number) {
 		const response = await fetch('/api/classify', {
@@ -14,23 +16,35 @@
 		return response.json();
 	}
 
-    let props = $props()
-	let input = $state('')
+	let { story_id } = $props();
+	let story = stories.find((x) => x.id == story_id);
+	if(story == undefined){throw Error(`Story n°${story_id} is missing !`)}
+	let question = questions.find((x) => x.id == story?.question_id);
+	if (question == undefined) {
+		throw Error(`The question n°${story?.question_id} of the story n°${story?.id} is missing !`);
+	}
+
+	let input = $state('');
 	let loading = $state(false);
 
 	function resolve_input() {
 		loading = true;
-		llm_classifier(input, props.question.id)
+		llm_classifier(input, question?.id || 0)
 			.then((result) => console.log(result))
+			.then()
 			.finally(() => (loading = false));
 	}
 </script>
 
-<p class="p-5">{props.story.text}</p>
+<p class="p-5">{story?.text}</p>
 
 <div class="flex items-start justify-center">
 	<form class="pr-3">
-		<textarea class="textarea w-xl" rows="5" placeholder={props.question.before_input} bind:value={input}></textarea>
+		<textarea
+			class="textarea w-xl"
+			rows="5"
+			placeholder={question.before_input}
+			bind:value={input}></textarea>
 	</form>
 
 	{#if loading}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-	import questions from '$lib/text/questions.json';
-	import story from '$lib/text/story.json';
 	import StoryBlock from '$lib/components/StoryBlock.svelte';
-
-	let theStory = story.find((x) => x.id == 0);
-	let theQuestion = questions.find((x) => x.id == theStory?.question_id);
 </script>
 
-<StoryBlock story={theStory} question={theQuestion} />
+<StoryBlock story_id={0} />
